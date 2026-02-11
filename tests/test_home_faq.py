686f793e74c8f199825b21c5350ec00132912621
Леонид Пионер <peon-russ@mail.ru>
2026-02-11 23:58:52 +0300

    Initial commit

---
+++ tests/test_home_faq.py
@@ -0,0 +1,43 @@
+import pytest
+import allure
+
+from selenium import webdriver
+from constants.constants import *
+from constants.locators import *
+from pages.base_page import BasePage
+
+
+class TestHomeFaq:
+    driver = None
+    sets = [(BaseLocators.Q0, BaseLocators.A0, Answers.AN0),
+            (BaseLocators.Q1, BaseLocators.A1, Answers.AN1),
+            (BaseLocators.Q2, BaseLocators.A2, Answers.AN2),
+            (BaseLocators.Q3, BaseLocators.A3, Answers.AN3),
+            (BaseLocators.Q4, BaseLocators.A4, Answers.AN4),
+            (BaseLocators.Q5, BaseLocators.A5, Answers.AN5),
+            (BaseLocators.Q6, BaseLocators.A6, Answers.AN6),
+            (BaseLocators.Q7, BaseLocators.A7, Answers.AN7)
+            ]
+
+    @classmethod
+    def setup_class(cls):
+        cls.driver = webdriver.Firefox()
+
+    @allure.title('Проверка текста всех пунктов "Вопросов о важном"')
+    @allure.description('На странице последовательно открываем все пункты аккордеона, получаем текст и сравнием его с образцом')
+    @pytest.mark.parametrize("accordion_number, accordion_text, expected_text", sets)
+    def test_home_faq(self, accordion_number, accordion_text, expected_text):
+
+        self.driver.get(Urls.BASE)
+        base_page = BasePage(self.driver)
+
+        base_page.scroll_to_home_faq()
+        base_page.click_accordion(accordion_number, accordion_text)
+        answer = base_page.get_answer_text(accordion_text)
+
+        assert answer == expected_text
+
+    @classmethod
+    def teardown_class(cls):
+        cls.driver.quit()
+
---
+++ tests/test_order_positive.py
@@ -0,0 +1,70 @@
+import pytest
+import allure
+
+from selenium import webdriver
+from constants.constants import *
+from constants.locators import *
+from pages import BasePage, OrderUserInfo, OrderRentInfo, OrderInfo
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class TestOrderPositive:
+    driver = None
+
+    @classmethod
+    def setup_class(cls):
+        cls.driver = webdriver.Firefox()
+
+    @allure.title('Проверка сценария заказа верхней кнопки"Заказать"')
+    @allure.description('Проверяем появление модального окна с сообщением об успешном заказе, проверяем, что при клик по логотипу самоката открывается главная страница Самоката')
+    def test_rent_up(self):
+        self.driver.get(Urls.BASE)
+        base_page = BasePage(self.driver)
+        base_page.click_rent_up()
+
+        user_info = OrderUserInfo(self.driver, UserData.IVAN)
+        user_info.set_user_info()
+
+        rent_info = OrderRentInfo(self.driver)
+        rent_info.set_order_info()
+        rent_info.confirmation()
+            
+        assert rent_info.check_order_succesful_popup()
+
+        rent_info.click_watch()
+
+        order_info = OrderInfo(self.driver)
+        order_info.click_scooter()
+
+        assert self.driver.current_url == Urls.BASE
+
+    @allure.title('Проверка сценария заказа нижней кнопки"Заказать"')
+    @allure.description('Проверяем появление модального окна с сообщением об успешном заказе, проверяем, что при клик по логотипу Яндекса открывается главная страница Яндекс.Дзен')
+    def test_rent_down(self):
+        self.driver.get(Urls.BASE)
+        base_page = BasePage(self.driver)
+
+        base_page.scroll_to_rent_down()
+        base_page.click_rent_down()
+
+        user_info = OrderUserInfo(self.driver, UserData.JOHN)
+        user_info.set_user_info()
+
+        rent_info = OrderRentInfo(self.driver)
+        rent_info.set_order_info()
+        rent_info.confirmation()
+            
+        assert rent_info.check_order_succesful_popup()
+
+        rent_info.click_watch()
+
+        order_info = OrderInfo(self.driver)
+        order_info.click_yandex_logo()
+        order_info.switch_to_new_window()
+                
+        assert order_info.check_url() == Urls.DZEN
+
+    @classmethod
+    def teardown_class(cls):
+        cls.driver.quit()
+
