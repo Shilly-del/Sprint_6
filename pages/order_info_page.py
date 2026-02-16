feff45523584d285e84ae3088d0afc04f067d4a0
Леонид Пионер <peon-russ@mail.ru>
2026-02-16 16:47:51 +0300

    В order_info_page удалены retern из кликов по логотипам

--- pages/order_info_page.py
+++ pages/order_info_page.py
@@ -13,14 +13,12 @@ class OrderInfoPage(BasePage):
     def click_scooter(self):
         self.wait_clickable(BaseLocators.SCOOTER)
         self.click(BaseLocators.SCOOTER)
-        return self
-
+        
     @allure.step('Кликаем по логотипу Яндекса') 
     def click_yandex_logo(self):
         self.wait_clickable(BaseLocators.LOGO)
         self.click(BaseLocators.LOGO)
-        return self
-    
+            
     def wait_invert(self):
         self.find_element(RentLocators.INVERT)
         return self
